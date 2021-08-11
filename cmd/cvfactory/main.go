53db657c625055c7526cbe75ae8b9a8551741a5b
Victor Iris <victormidp@gmail.com>
2021-08-11 02:45:50 -0500

    fix: fix structs warning

--- cmd/cvfactory/main.go
+++ cmd/cvfactory/main.go
@@ -10,46 +10,46 @@ import (
 )
 
 type EducationItem struct {
-	Title           string `json: "title"`
-	Description     string `json: "description"`
-	InstitutionName string `json: "institutionName"`
-	Location        string `json: "location"`
-	StartDate       string `json: "startDate"`
-	EndDate         string `json: "endDate"`
-	ReferenceId     string `json: "referenceId"`
-	Url             string `json: "url"`
+	Title           string `json:"title"`
+	Description     string `json:"description"`
+	InstitutionName string `json:"institutionName"`
+	Location        string `json:"location"`
+	StartDate       string `json:"startDate"`
+	EndDate         string `json:"endDate"`
+	ReferenceId     string `json:"referenceId"`
+	Url             string `json:"url"`
 }
 
 type List struct {
-	Name    string `json: "name"`
-	Content string `json: "content"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
 }
 
 type Skill struct {
-	Name        string `json: "name"`
-	Description string `json: "description"`
-	Proficiency string `json: "proficiency"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Proficiency string `json:"proficiency"`
 }
 
 type Job struct {
-	Title       string `json: "title"`
-	Description string `json: "description"`
-	CompanyName string `json: "companyName"`
-	Location    string `json: "location"`
-	StartDate   string `json: "startDate"`
-	EndDate     string `json: "endDate"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	CompanyName string `json:"companyName"`
+	Location    string `json:"location"`
+	StartDate   string `json:"startDate"`
+	EndDate     string `json:"endDate"`
 }
 
 type UserData struct {
-	Firstname      string          `json: "firstname"`
-	Lastname       string          `json: "lastname"`
-	Email          string          `json: "email"`
-	Phone          string          `json: "phone"`
-	Address        string          `json: "address"`
-	Jobs           []Job           `json: "jobs"`
-	Skills         []Skill         `json: "skills"`
-	Lists          []List          `json: "lists"`
-	EducationItems []EducationItem `json: "educationItems"`
+	Firstname      string          `json:"firstname"`
+	Lastname       string          `json:"lastname"`
+	Email          string          `json:"email"`
+	Phone          string          `json:"phone"`
+	Address        string          `json:"address"`
+	Jobs           []Job           `json:"jobs"`
+	Skills         []Skill         `json:"skills"`
+	Lists          []List          `json:"lists"`
+	EducationItems []EducationItem `json:"educationItems"`
 }
 
 func FillTemplate(dataPath string, templatePath string) {
